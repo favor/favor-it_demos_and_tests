13a65119a38d9b8b4248bf4059cc3745d36b0ecb
Pete Field <pedalpete@hotmail.com>
2014-12-09 20:42:08 +1100

    fixed i2c_accel object

--- i2c_accelerometer/favorit.js
+++ i2c_accelerometer/favorit.js
@@ -1,7 +1,7 @@
 /* Demo and test of i2c accelerometer
 */
 {   name:"Test-accelerometer",
-    components : [{type:"accelerometer", interface:"i2c", init: [
+    components: [{type:"accelerometer", interface:"i2c", init: [
         {cmd:'write', byte: 0x2D, bytes: [1 << 3]}
         {cmd:'write', byte:0x31, bytes:[0x09]},
         {cmd:'write', byte: 0x2c, bytes: [8 + 2 + 1], wait: 10}
