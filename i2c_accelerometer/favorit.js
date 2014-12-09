bef7bd68d7f81d5d81242b140f2108543767d42d
Pete Field <pedalpete@hotmail.com>
2014-12-09 20:56:17 +1100

    added i2c_path

--- i2c_accelerometer/favorit.js
+++ i2c_accelerometer/favorit.js
@@ -1,6 +1,7 @@
 /* Demo and test of i2c accelerometer
 */
 module.exports = {   name:"Test-accelerometer",
+    i2c_path: "/dev/i2c/-1",
     components: [{type:"accelerometer", 
     interface:"i2c", 
     init: [{cmd:'write', byte: 0x2D, bytes: [1 << 3]},
