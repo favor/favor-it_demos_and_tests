fc638a505f2e9de1fbe86dc0ba61d3d190d59511
Pete Field <pedalpete@hotmail.com>
2014-12-13 10:53:48 +1100

    forgot address

--- i2c_accelerometer/favorit.js
+++ i2c_accelerometer/favorit.js
@@ -4,6 +4,7 @@ module.exports = {   name:"Test-accelerometer",
     i2c_path: "/dev/i2c/1",
     components: [{type:"accelerometer", 
     interface:"i2c", 
+    address: 0x53,
     init: [{cmd:'write', byte: 0x2D, bytes: [1 << 3]},
         {cmd:'write', byte:0x31, bytes:[0x09]},
         {cmd:'write', byte: 0x2c, bytes: [8 + 2 + 1], wait: 10}
