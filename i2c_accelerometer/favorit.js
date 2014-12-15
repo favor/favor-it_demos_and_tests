2d513eeac7fdaaa1c0a38ca6dca4390320bbf7f5
Pete Field <pedalpete@hotmail.com>
2014-12-15 19:40:53 +1100

    added post_action to accelerometer

--- i2c_accelerometer/favorit.js
+++ i2c_accelerometer/favorit.js
@@ -1,5 +1,15 @@
 /* Demo and test of i2c accelerometer
 */
+
+var format_accel = function(val){
+    var meterPerSecSec=[0.0,0.0,0.0];
+    var accelScaleFavor=[0.0,0.0,0.0];
+    runTimeAccelBias = [0, 0, 0];
+    for (var axis = XAXIS; axis <= ZAXIS; axis++) {
+				meterPerSecSec[axis] = res.readInt16LE(axis*2) * accelScaleFactor[axis] + runTimeAccelBias[axis];
+			}
+    return meterPerSecSec;
+}
 module.exports = {   name:"Test-accelerometer",
     i2c_path: "/dev/i2c/1",
     components: [{type:"accelerometer", 
@@ -9,6 +19,6 @@ module.exports = {   name:"Test-accelerometer",
         {cmd:'write', byte:0x31, bytes:[0x09]},
         {cmd:'write', byte: 0x2c, bytes: [8 + 2 + 1], wait: 10}
         ], 
-    get:{cmd: 'read', byte: 0x32, bytes: 6}
+    get:{cmd: 'read', byte: 0x32, bytes: 6, post_action: format_accel}
     }]
 }
